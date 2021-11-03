2672fdbfffbed26e93fe654b22a371487fd24b40
User <User@DESKTOP-C7DGT0E>
2021-11-03 14:27:21 +0300

    Добавьте файлы проекта.

---
+++ Tashoglo.3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tashoglo._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 1; i <= 1000; i++)
+                {
+                    int sum = 0;
+                    for (int a = 1; a <= i; a++)
+                    {
+                        int x = i % a;
+                        if (x == 0)
+                            sum++;
+                    }
+                    if (sum == 5)
+                        Console.WriteLine(i);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        } 
+    }
+}
